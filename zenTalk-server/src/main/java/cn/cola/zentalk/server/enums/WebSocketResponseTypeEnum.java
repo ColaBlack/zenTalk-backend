9eb33743064191113c876e11b15fd0fc3e78b997
ColaBlack <2698360477@qq.com>
2024-11-20 19:41:25 +0800

    feat 完成websocket的模版

--- zenTalk-server/src/main/java/cn/cola/zentalk/server/enums/WebSocketResponseTypeEnum.java
+++ zenTalk-server/src/main/java/cn/cola/zentalk/server/enums/WebSocketResponseTypeEnum.java
@@ -1,7 +1,6 @@
 package cn.cola.zentalk.server.enums;
 
 
-import cn.cola.zentalk.server.model.vo.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -18,60 +17,60 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @Getter
 public enum WebSocketResponseTypeEnum {
-    /**
-     * 登录二维码返回
-     */
-    SENT_LOGIN_URL(1, "登录二维码返回", WSLoginUrl.class),
-
+//    /**
+//     * 登录二维码返回
+//     */
+//    SENT_LOGIN_URL(1, "登录二维码返回", WSLoginUrl.class),
+//
     /**
      * 用户扫描二维码成功等待授权
      */
-    LOGIN_SCAN_SUCCESS(2, "用户扫描成功等待授权", null),
-
-    /**
-     * 用户登录成功返回用户信息
-     */
-    LOGIN_SUCCESS(3, "用户登录成功返回用户信息", WSLoginSuccess.class),
-
-    /**
-     * 新消息
-     */
-    MESSAGE(4, "新消息", WSMessage.class),
-
-    /**
-     * 上下线通知
-     */
-    ONLINE_OFFLINE_NOTIFY(5, "上下线通知", WSOnlineOfflineNotify.class),
-
-    /**
-     * 使前端的token失效，意味着前端需要重新登录
-     */
-    INVALIDATE_TOKEN(6, "使前端的token失效，意味着前端需要重新登录", null),
-
-    /**
-     * 拉黑用户
-     */
-    BLACK(7, "拉黑用户", WSBlack.class),
-
-    /**
-     * 消息标记
-     */
-    MARK(8, "消息标记", WSMsgMark.class),
-
-    /**
-     * 消息撤回
-     */
-    RECALL(9, "消息撤回", WSMsgRecall.class),
-
-    /**
-     * 好友申请
-     */
-    APPLY(10, "好友申请", WSFriendApply.class),
-
-    /**
-     * 成员变动
-     */
-    MEMBER_CHANGE(11, "成员变动", WSMemberChange.class);
+    LOGIN_SCAN_SUCCESS(2, "用户扫描成功等待授权", null);
+//
+//    /**
+//     * 用户登录成功返回用户信息
+//     */
+//    LOGIN_SUCCESS(3, "用户登录成功返回用户信息", WSLoginSuccess.class),
+//
+//    /**
+//     * 新消息
+//     */
+//    MESSAGE(4, "新消息", WSMessage.class),
+//
+//    /**
+//     * 上下线通知
+//     */
+//    ONLINE_OFFLINE_NOTIFY(5, "上下线通知", WSOnlineOfflineNotify.class),
+//
+//    /**
+//     * 使前端的token失效，意味着前端需要重新登录
+//     */
+//    INVALIDATE_TOKEN(6, "使前端的token失效，意味着前端需要重新登录", null),
+//
+//    /**
+//     * 拉黑用户
+//     */
+//    BLACK(7, "拉黑用户", WSBlack.class),
+//
+//    /**
+//     * 消息标记
+//     */
+//    MARK(8, "消息标记", WSMsgMark.class),
+//
+//    /**
+//     * 消息撤回
+//     */
+//    RECALL(9, "消息撤回", WSMsgRecall.class),
+//
+//    /**
+//     * 好友申请
+//     */
+//    APPLY(10, "好友申请", WSFriendApply.class),
+//
+//    /**
+//     * 成员变动
+//     */
+//    MEMBER_CHANGE(11, "成员变动", WSMemberChange.class);
 
     /**
      * 响应类型
